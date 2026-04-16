c38eb5dc59a8f834f4d4e8df8fc789f7b4fe6240
LuizFernand018 <luiz.00000859569@unicap.br>
2026-04-16 09:31:57 -0300

    Add ContaPoupanca.java

---
+++ ContaPoupanca.java
@@ -0,0 +1,21 @@
+package Exercicio_1;
+
+public class ContaPoupanca extends Conta {
+    @Override
+    String getTipo(){
+        return "Conta Poupanca";
+    }
+
+    @Override
+    public double sacar(double valor){
+        if(getSaldo() >= valor){
+            double novoSaldo = getSaldo() - valor;
+            setSaldo(novoSaldo);
+            return novoSaldo;
+        }
+        else{
+            System.out.println("Saldo Insuficiente");
+            return getSaldo();
+        }
+    }
+}
